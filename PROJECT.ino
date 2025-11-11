88fc0fd753d7e5ef287bff3f96d35fe209d2dcec
Nguyen Tran Minh Duc <redgreenblue@outlook.com.vn>
2025-11-11 14:20:23 +0200

    Perfect.

---
+++ PROJECT.ino
@@ -0,0 +1,191 @@
+
+#include "CallbackButton.h" // Library modified for PULLUP
+
+// Tones
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
+const int enableButtonPin = 2;
+const int disableButtonPin = 3;
+const int vibratorPin = 8;
+const int piezoPin = 9;
+const int ledPin = 13;
+
+CallbackButton enableButton = CallbackButton(enableButtonPin);
+CallbackButton disableButton = CallbackButton(disableButtonPin);
+
+bool enabled = false;
+unsigned long activatedTime = 0;
+unsigned long activationDelay = 0;
+uint8_t toneIndex = 0;
+
+typedef struct {
+  uint16_t tone;
+  uint16_t time;
+} ringtone_t;
+
+ringtone_t ringtone[] = {
+  {NOTE_E5,  187},
+  {NOTE_D5, 374},
+  {NOTE_FS4, 749},
+  {NOTE_GS4, 1124},
+  {NOTE_CS5, 1311},
+  {NOTE_B4, 1498},
+  {NOTE_D4, 1873},
+  {NOTE_E4, 2248},
+  {NOTE_B4, 2435},
+  {NOTE_A4, 2622},
+  {NOTE_CS4, 2997},
+  {NOTE_E4, 3372},
+  {NOTE_A4, 4122},
+};
+uint8_t ringtoneSize = sizeof(ringtone) / sizeof(*ringtone);
+
+#define rot8(x,k) (((x) << (k))|((x) >> (8 - (k))))
+uint8_t jsf8_random() {
+	static uint8_t a = 0xf1;
+	static uint8_t b = 0xee, c = 0xee, d = 0xee;
+
+	uint8_t e = a - rot8(b, 1);
+	a = b ^ rot8(c, 4);
+	b = c + d;
+	c = d + e;
+	return d = e + a;
+}
+
+void enableButtonHandler(int port, int clickType) {
+  if (!enabled) {
+    Serial.print("Enable\n");
+    enabled = true;
+    activatedTime = millis();
+    activationDelay = 5000 + jsf8_random() * 20;
+    toneIndex = 0;
+  }
+}
+void disableButtonHandler(int port, int clickType) {
+  Serial.print("Disable\n");
+  enabled = false;
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(enableButtonPin, INPUT_PULLUP);
+  pinMode(disableButtonPin, INPUT_PULLUP);
+  pinMode(vibratorPin, OUTPUT);
+  pinMode(piezoPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  enableButton.setCallbackFunction(enableButtonHandler);
+  disableButton.setCallbackFunction(disableButtonHandler);
+}
+
+void loop() {
+  enableButton.checkButtonState();
+  disableButton.checkButtonState();
+  bool actuallyEnabled = enabled && (millis() - activatedTime) >= activationDelay;
+  bool ledState = enabled ? (millis() / 250 % 2) : 1;
+  bool vibrate = actuallyEnabled ? (millis() / 1000 % 2) : 0;
+  digitalWrite(ledPin, ledState);
+  digitalWrite(vibratorPin, vibrate);
+  
+  static unsigned long toneStartTime = activatedTime + activationDelay;
+  if (actuallyEnabled) {
+    if (millis() - toneStartTime > ringtone[toneIndex].time) {
+      toneIndex += 1;
+      if (toneIndex == ringtoneSize) {
+        // Reset ringtone
+        toneIndex = 0;
+        toneStartTime = millis();
+      }
+    }
+    tone(piezoPin, ringtone[toneIndex].tone);
+  } else {
+    noTone(piezoPin);
+  }
+}
